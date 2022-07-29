f49b7c1adb166afd973b783218013d5d2d78decd
NGUYEN_CHIEN <nguyenchien.0790@gmail.com>
2022-07-29 15:41:16 +0700

    Lan 1

---
+++ src/rikkei/academy/Student.java
@@ -0,0 +1,30 @@
+package rikkei.academy;
+
+public class Student {
+    private String name = "John";
+    private String classes = "CO2";
+    public Student(){
+
+    }
+    public Student(String name, String classes){
+        this.name =name;
+        this.classes=classes;
+
+    }
+    public void setName(String name){
+        this.name=name;
+
+    }
+    public void setClasses(String classes){
+        this.classes=classes;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
---
+++ src/rikkei/academy/Test.java
@@ -0,0 +1,12 @@
+package rikkei.academy;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println("Truoc khi gan : "+student);
+        student.setName("Nguyen Chien");
+        student.setClasses("JV062022");
+        System.out.println("Sau khi gan : "+student);
+
+    }
+}
